cb32e2e479a6298536d8a937a7322779fd546641
cs-yash-patodkar <ypatodkar@fortinet-us.com>
2023-06-13 11:36:26 +0530

    Initial commit

---
+++ view.controller.js
@@ -0,0 +1,40 @@
+'use strict';
+(function () {
+    angular
+        .module('cybersponse')
+        .controller('situationalAwarenessWidget100Ctrl', situationalAwarenessWidget100Ctrl);
+
+    situationalAwarenessWidget100Ctrl.$inject = ['$scope', 'PagedCollection'];
+
+    function situationalAwarenessWidget100Ctrl($scope, PagedCollection) {
+
+        $scope.widgetData = {};
+        $scope.sizeType = '';
+
+        init();
+        function init() {
+            fetchJsonData();
+        }
+
+
+        function fetchJsonData() {
+            var filters = {
+                query: $scope.config.query
+            };
+            var pagedTotalData = new PagedCollection($scope.config.customModule, null, null);
+            pagedTotalData.loadByPost(filters).then(function () {
+                if (pagedTotalData.fieldRows.length === 0) {
+                    $scope.filterValidation = true;
+                    return;
+                }
+                $scope.widgetData = pagedTotalData.fieldRows[0][$scope.config.customModuleField].value;
+                if ('data' in $scope.widgetData) {
+                    $scope.sizeType = 'large';
+                }
+                else {
+                    $scope.sizeType = 'small';
+                }
+            })
+        }
+    }
+})();
